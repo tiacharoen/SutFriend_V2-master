a8f75c609a6157e83f257e3d71de9ff0144d3f20
tia <tiacharoen@yahoo.com>
2018-08-29 08:46:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# SutFriend
+Workshop at Sut
+## Workshop กับ มาสเตอร์ อึ่ง ที่ ม.สุรนารี
+
+[![Screen_Shot_2561-08-26_at_09.23.13.png](https://s26.postimg.cc/cs6tyyf89/Screen_Shot_2561-08-26_at_09.23.13.png)](https://postimg.cc/image/it4iw11ud/)
+
+[![Screen_Shot_2561-08-26_at_09.23.25.png](https://s26.postimg.cc/qw7r4gl1l/Screen_Shot_2561-08-26_at_09.23.25.png)](https://postimg.cc/image/lksujqyyt/)
+
+[![Screen_Shot_2561-08-27_at_15.11.42.png](https://s26.postimg.cc/az8z7jiax/Screen_Shot_2561-08-27_at_15.11.42.png)](https://postimg.cc/image/f8dp9plk5/)
+
+### Workshop With มาสเตอร์ อึ่ง
+
+## ต้องการรายละเอียดเพิ่ม หรือ ต้องการ ปรึกษาการทำ โปรเจคแอนดรอยด์
+### ติดต่อมาสเตอร์ อึ่ง เลย ที่
+
+[![IMG_6065.jpg](https://s26.postimg.cc/kajrs6fbt/IMG_6065.jpg)](https://postimg.cc/image/7j5llo5jp/)
+
+
+https://www.facebook.com/android.Training.by.EWTC
+
+http://line.me/ti/p/XI-Ksj7Jzq
+
+[![Line-_QR.jpg](https://s26.postimg.cc/dwuoozv15/Line-_QR.jpg)](https://postimg.cc/image/mrvizijth/)
+
+http://androidthai.in.th
+
+http://swiftcodingthai.com
+
+phrombutr@gmail.com
+
+Mobile 0818595309
+
---
+++ app/src/main/java/masterung/androidthai/in/th/sutfriend/ServiceActivity.java
@@ -0,0 +1,44 @@
+package masterung.androidthai.in.th.sutfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+
+//        Exit Controller
+        exitController();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentFragmentService, new ListUserFragment())
+                    .commit();
+        }
+
+
+    }
+
+    private void exitController() {
+        TextView textView = findViewById(R.id.txtExit);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                firebaseAuth.signOut();
+                finish();
+
+
+            }
+        });
+    }
+}
